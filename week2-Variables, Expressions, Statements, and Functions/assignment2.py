457f9fde9330996b29153befe3f70e001693d82f
evans <ekioko404@gmail.com>
2023-12-27 00:00:42 -0800

    Unit 2 resources added

---
+++ week2-Variables, Expressions, Statements, and Functions/assignment2.py
@@ -0,0 +1,5 @@
+import math  # import math module for math calculations
+
+def print_circum(radius): # function to calculate circumference of circle
+  circum = 2 * math.pi * radius # formula to calculate circumference
+  
